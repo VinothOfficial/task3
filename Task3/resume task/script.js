617c132f7b7f4ef3d5520dcb3b2d5129efae1e88
VinothOfficial <106010245+VinothOfficial@users.noreply.github.com>
2022-08-26 15:59:06 +0530

    Add files via upload

---
+++ Task3/resume task/script.js
@@ -0,0 +1,47 @@
+let myResume={
+  "basics": {
+    "name": "VINOTH",
+    "email": "Vkofficial1710@gmail.com",
+    "phone": 7904044202,
+    "degree": "B.sc",
+    "location": {
+   
+      "postalCode": 600030,
+      "city": "Chennai",
+      "state": "Tamilnadu",
+      "country": "India"
+    },
+    
+  },
+  
+  "education": [
+    {
+      "institution": "gurunanak college",
+      "department": "computer science",
+      "studyType": "fulltime",
+      "batch start year": 2014,
+      "batch end year": 2017,
+      "gpa": 7.5,
+    }
+  ],
+  "skills": [
+    {
+      "name": "javascript",
+      "level": "beginer",
+      "project": [
+        "automatic attendance maintaing system using python language"
+      ]
+    }
+  ],
+  "languages": [
+    {
+      "language": "Tamil,Enlish",
+    }
+  ],
+  "interests": [
+    {
+      "name": "script writter,youtuber,",
+    }
+  ]
+}
+console.log(myResume);
